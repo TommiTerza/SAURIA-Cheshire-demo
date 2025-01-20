68cfe50dc830f7ff0416c49bd2fc44cdd32206ca
TommiTerza <s317650@studenti.polito.it>
2025-01-20 17:15:27 +0100

    bender dev

--- sauria_demo.mk
+++ sauria_demo.mk
@@ -31,6 +31,6 @@ include $(shell bender path cheshire)/cheshire.mk
 .PHONY: hw-all
 hw-all:
 	$(MAKE) -B chs-hw-all
-	vlog $(SAURIA_DEMO_ROOT)/pulp_platform/axi/src/axi_pkg.sv
-	vlog $(SAURIA_DEMO_ROOT)/RTL/src/sauria_pkg.sv
-	vlog -f $(SAURIA_DEMO_ROOT)/RTL/filelist.f
+	vlog $(SAURIA_ROOT)/pulp_platform/axi/src/axi_pkg.sv
+	vlog $(SAURIA_ROOT)/RTL/src/sauria_pkg.sv
+	vlog -f $(SAURIA_ROOT)/RTL/filelist.f
