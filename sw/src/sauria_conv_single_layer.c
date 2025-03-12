6644fa51d614eb7e012809dcb50e5b747616f021
TommiTerza <s317650@studenti.polito.it>
2025-03-12 10:15:26 +0100

    Update

--- sw/src/sauria_conv_single_layer.c
+++ sw/src/sauria_conv_single_layer.c
@@ -47,13 +47,10 @@
 #define EXTERNAL_IRQ_ENABLE_ADDRESS 0x00000800
 
 /* Variables */
-// static dif_rv_plic_t plic0;
 char ret_reg_hex_conv[13]; 
 char ret_mem_hex_conv[13];
 uint32_t ret_mem;
 uint32_t ret_reg;
-//sauria_t sauria;
-//bool t;
 
 int main(void) {
     /* Immediately return an error if DMA is not present */
@@ -64,27 +61,17 @@ int main(void) {
     uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
     uart_init(&__base_uart, reset_freq, __BOOT_BAUDRATE);
 
-    /* Set up the PLIC */
-    /*
-    asm volatile("csrw  mstatus, %0\n" : : "r"(GLOBAL_IRQ_ENABLE_ADDRESS));     // Set global interrupt enable in CVA6 csr
-    asm volatile("csrw  mie, %0\n"     : : "r"(EXTERNAL_IRQ_ENABLE_ADDRESS));     // Set external interrupt enable in CVA6 csr
-    
-    mmio_region_t plic_base_addr = mmio_region_from_addr(0x04000000);
-    t = dif_rv_plic_init(plic_base_addr, &plic0);
-    t = dif_rv_plic_irq_set_priority(&plic0, mbox_id, prio);
-    t = dif_rv_plic_irq_set_enabled(&plic0, mbox_id, 0, kDifToggleEnabled);
-    plic_Init();
-    plic_irq_set_priority(SAURIA_INTR, 1);
-    plic_irq_set_enabled(SAURIA_INTR, kPlicToggleEnabled);
-    plic_assign_external_irq_handler( SAURIA_INTR, (void *) &handler_irq_sauria);
-    
-
-    sauria.base_addr = mmio_region_from_addr((uintptr_t)SAURIA_REG_START_ADDRESS);
-    */
-
     /* Write SAURIA's IFMAP SRMA via the iDMA */
     int8_t *inputs_ptr = &input_tensor[0][0][0];
     sys_dma_memcpy((uintptr_t)(void *)(SAURIA_MEM_START_ADDRESS + SAURIA_SRAMA_OFFSET), (uintptr_t)(void *)inputs_ptr, sizeof(AB_c*A_h_padded*A_w_padded));
 
+    ret_mem = *(volatile uint32_t *) (SAURIA_MEM_START_ADDRESS + SAURIA_SRAMA_OFFSET);
+
+    /* Convert the value to a hex-formatted string */
+    uint32_to_hex_string(ret_mem, ret_mem_hex_conv);  // Convert integer to hex string
+
+    /* Print the value */
+    PRINTF(ret_mem_hex_conv);
+    
     return 0;
 }
